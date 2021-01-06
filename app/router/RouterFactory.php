e38287c24eeac9513fc48805c386b05a59db72e6
David Grudl <david@grudl.com>
2021-01-06 04:53:51 +0100

    updated to Nette 3.1

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -4,21 +4,15 @@ declare(strict_types = 1);
 
 namespace App;
 
-use Nette\Application\IRouter;
-use Nette\Application\Routers\Route;
+use Nette;
 use Nette\Application\Routers\RouteList;
 
 class RouterFactory
 {
-
-	/**
-	 * @return IRouter
-	 */
-	public static function createRouter(): IRouter
+	public static function createRouter(): RouteList
 	{
 		$router = new RouteList;
-		$router[] = new Route('<presenter>/<action>', 'Homepage:default');
-
+		$router->addRoute('<presenter>/<action>', 'Homepage:default');
 		return $router;
 	}
 }
